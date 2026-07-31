3651e283a0278aaf372c36f73f281bf125f3f709
nicholastey6 <163625427+nicholastey6@users.noreply.github.com>
2026-07-31 21:43:27 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,48 @@
 
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
+from langchain.chat_models import ChatOpenAI
+
+from tools.database_tool import search_failure
+
+
+def database_search(text):
+
+    result = search_failure(text)
+
+    return str(result)
+
+
+
+tools = [
+
+Tool(
+    name="Failure Database Search",
+    func=database_search,
+    description=
+    """
+    Search previous semiconductor failures
+    and return similar cases.
+    """
+)
+
+]
+
+
+llm = ChatOpenAI(
+    model="gpt-4.1-mini",
+    temperature=0
+)
+
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent="zero-shot-react-description",
+    verbose=True
+)
+
+
+def ask_agent(question):
+
+    return agent.run(question)
