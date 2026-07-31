ad08fe848a474e80de24ff2b4befa776ac758c92
nicholastey6 <163625427+nicholastey6@users.noreply.github.com>
2026-07-31 21:43:10 +0800

    Update database_tool.py

--- tools/database_tool.py
+++ tools/database_tool.py
@@ -1,1 +1,27 @@
+import sqlite3
 
+
+def search_failure(keyword):
+
+    conn = sqlite3.connect(
+        "database/failures.db"
+    )
+
+    cursor = conn.cursor()
+
+    query = """
+    SELECT *
+    FROM failures
+    WHERE defect LIKE ?
+    """
+
+    cursor.execute(
+        query,
+        ('%' + keyword + '%',)
+    )
+
+    result = cursor.fetchall()
+
+    conn.close()
+
+    return result
